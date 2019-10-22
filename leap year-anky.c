fba0e4edaafcc321dbcc412874cb9ecdf5781422
anky19 <42665221+anky19@users.noreply.github.com>
2019-10-22 23:29:41 +0530

    Create leap year-anky.c

---
+++ leap year-anky.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{
+	int a;
+	printf("to check wheather its leap year or not \n");
+	scanf("%d",&a);
+ if(a%4==0)
+{
+ printf("it is leap year");
+}
+else
+{
+	printf("its not");
+	}	
+   getch();
+   return 0;
+}
+
+}

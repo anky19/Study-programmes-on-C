51b1b4e587e0ae96b9630941b0a9266860abb897
anky19 <42665221+anky19@users.noreply.github.com>
2019-10-22 23:25:10 +0530

    Create anky-switchcase.c

---
+++ anky-switchcase.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	switch(a)
+{
+
+	case 1:
+		printf("sunday");
+		break;
+		case 2:
+			printf("monday");
+			break;
+			case 3:
+				printf("tuesday");
+				break;
+				case 4:
+					printf("wednesday");
+					break;
+				default:
+					printf("no output");
+					break;
+					getch();
+					return 0;
+}
+				
+	
+}
